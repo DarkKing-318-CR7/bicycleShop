a0a96db965c207e7e89861e2055a7df8d8646356
Dương Văn Đức <ducdv1631@ut.edu.vn>
2026-04-27 19:03:43 +0700

    update code duc

--- seller/add-bike.php
+++ seller/add-bike.php
@@ -386,6 +386,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     <li class="nav-item"><a class="nav-link" href="#contact">Liên hệ</a></li>
                 </ul>
                 <div class="d-flex flex-column flex-lg-row gap-2">
+                    <a href="my-bikes.php" class="btn btn-outline-dark">Tin đăng</a>
+                    <a href="orders.php" class="btn btn-outline-dark">Đơn mua</a>
                     <a href="<?= e(getUserHomeLink($userRole)) ?>" class="btn btn-outline-dark"><?= e($sellerName) ?></a>
                     <a href="../logout.php" class="btn btn-success">Đăng xuất</a>
                 </div>
